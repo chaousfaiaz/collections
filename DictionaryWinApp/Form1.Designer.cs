b7c0cde9a1e90024985087ce48840ddadf1f366d
chaousfaiaz <53284875+chaousfaiaz@users.noreply.github.com>
2021-04-12 21:06:28 -0400

    Add files via upload

---
+++ DictionaryWinApp/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace DictionaryWinApp
+{
+    partial class frmDictionary
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstKeysAndValues = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstValues = new System.Windows.Forms.ListBox();
+            this.lstKeys = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(529, 49);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(245, 25);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Keys && Values";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lstKeysAndValues
+            // 
+            this.lstKeysAndValues.FormattingEnabled = true;
+            this.lstKeysAndValues.ItemHeight = 16;
+            this.lstKeysAndValues.Location = new System.Drawing.Point(529, 78);
+            this.lstKeysAndValues.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstKeysAndValues.Name = "lstKeysAndValues";
+            this.lstKeysAndValues.Size = new System.Drawing.Size(245, 324);
+            this.lstKeysAndValues.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(277, 49);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(243, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Values";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(27, 49);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Keys";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lstValues
+            // 
+            this.lstValues.FormattingEnabled = true;
+            this.lstValues.ItemHeight = 16;
+            this.lstValues.Location = new System.Drawing.Point(277, 78);
+            this.lstValues.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstValues.Name = "lstValues";
+            this.lstValues.Size = new System.Drawing.Size(243, 324);
+            this.lstValues.TabIndex = 7;
+            // 
+            // lstKeys
+            // 
+            this.lstKeys.FormattingEnabled = true;
+            this.lstKeys.ItemHeight = 16;
+            this.lstKeys.Location = new System.Drawing.Point(27, 78);
+            this.lstKeys.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstKeys.Name = "lstKeys";
+            this.lstKeys.Size = new System.Drawing.Size(243, 324);
+            this.lstKeys.TabIndex = 5;
+            // 
+            // frmDictionary
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lstKeysAndValues);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstValues);
+            this.Controls.Add(this.lstKeys);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "frmDictionary";
+            this.Text = "Dictionary";
+            this.Load += new System.EventHandler(this.frmDictionary_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstKeysAndValues;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstValues;
+        private System.Windows.Forms.ListBox lstKeys;
+    }
+}
+
---
+++ DictionaryWinApp/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DictionaryWinApp
+{
+    public partial class frmDictionary : Form
+    {
+        public frmDictionary()
+        {
+            InitializeComponent();
+        }
+
+        private void frmDictionary_Load(object sender, EventArgs e)
+        {
+
+            // The dictionary of digit names.
+            //Dictionary<int, string> groceryCollection = new Dictionary<int, string>();
+            SortedDictionary<int, string> groceryCollection = new SortedDictionary<int, string>();
+
+
+            groceryCollection.Add(3, "Milk");
+            groceryCollection.Add(6, "Eggs");
+            groceryCollection.Add(4, "Coffee");
+            groceryCollection.Add(5, "Juice");
+            groceryCollection.Add(0, "Sugar");
+            groceryCollection.Add(1, "Bread");
+            groceryCollection.Add(2, "Oranges");
+            groceryCollection.Add(7, "Bananas");
+            groceryCollection.Add(8, "Waffles");
+            groceryCollection.Add(9, "Cheese");                       
+
+            // Display the keys and values.
+            foreach (int index in groceryCollection.Keys)
+            {
+                lstKeys.Items.Add(index);
+                lstValues.Items.Add(groceryCollection[index]);
+                lstKeysAndValues.Items.Add(index.ToString() + ": " + groceryCollection[index]);
+            }
+        }
+    }
+}
